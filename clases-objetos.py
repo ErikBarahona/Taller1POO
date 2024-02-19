f28a2f642659049c0f09ed85da4824c579369ec7
ErikBarahona <149337398+ErikBarahona@users.noreply.github.com>
2024-02-19 11:28:05 -0500

    Update clases-objetos.py

--- clases-objetos.py
+++ clases-objetos.py
@@ -1,10 +1,16 @@
 class computadora:
   def __init__(self):
-    print "Tipo de computador"
-
+    print ("Tipo de computador")
+  def tipo(self,mensaje):
+    print mensaje
+    
 Desktop = computadora()
+Desktop.tipo('soy un Desktop')
 AIO = computadora()
+AIO.tipo('soy un All in one')
 Laptop = computadora()
+Laptop.tipo('soy una Laptop')
 Notebook = computadora()
+Notebook.tipo('soy un Notebook')
 
 
