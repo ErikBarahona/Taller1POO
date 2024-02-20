57ac36b1735545864e1b6f422442589966be4c9d
ErikBarahona <149337398+ErikBarahona@users.noreply.github.com>
2024-02-19 19:55:01 -0500

    Create abstraccion.py

---
+++ abstraccion.py
@@ -0,0 +1,7 @@
+class computadora:
+  def laptop(self, color, pulgadas, velocidad, memoria):
+    self.color = color
+    self.pulgadas = pulgadas
+    self.velocidad = velocidad
+    self.memoria = memoria
+    
