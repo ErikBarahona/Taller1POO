f1cf0167dd56dd888d52ef4c70bb97f5f9871c7b
ErikBarahona <149337398+ErikBarahona@users.noreply.github.com>
2024-02-19 20:12:41 -0500

    Update abstraccion.py

--- abstraccion.py
+++ abstraccion.py
@@ -1,7 +1,22 @@
 class computadora:
-  def laptop(self, color, pulgadas, velocidad, memoria):
+  def __init__(self, color, pulgadas, velocidad, memoria):
     self.color = color
     self.pulgadas = pulgadas
     self.velocidad = velocidad
     self.memoria = memoria
+
+    def caracteristicas(self):
+      pass
+
+
+class hp(computadora):
+  def caracteristicas(self):
+    print("Estoy usando la computadora laptop de color " + self.color + " de " + self.pulgadas + " pulgadas, con una velocidad de " +self.velocidad + " GHz y con un almacenamiento de " + self.memoria)
+
+
+hp = hp("azul", "15", "560", "16" )
+
+
+hp.caracteristicas()
+
     
