1e69a258d6fb2379e4b8fd4e0788bf0b145479cb
ErikBarahona <149337398+ErikBarahona@users.noreply.github.com>
2024-02-19 21:40:08 -0500

    Create Atributos.py

---
+++ Atributos.py
@@ -0,0 +1,8 @@
+class computadora:
+    pass
+
+Laptop = computadora()
+Laptop.tamaño = "15"
+Laptop.color = "marrón"
+
+print("El color de la computadora laptop es ", Laptop.tamaño , "y el color", Laptop.color)
